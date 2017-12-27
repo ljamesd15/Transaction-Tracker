7e2385e853c3ec2da0035508e3e292179569cb5a
James Davidson <ljamesdavids@gmail.com>
2017-12-26 21:32:33 -0800

    SQLite database update

--- src/model/Transaction.java
+++ src/model/Transaction.java
@@ -2,22 +2,25 @@ package model;
 
 import java.time.LocalDate;
 
+/**
+ * Holds all the information of a certain transaction.
+ * @author L. James Davidson
+ */
 public class Transaction {
 
-	private String location;
-	private double amount;
-	private LocalDate date;
-	private CategoryTag category;
-	private String memo;
+	private final String description;
+	private final double amount;
+	private final LocalDate date;
+	private final String category;
+	private final String memo;
 	
 	// AF: 
-	// The location of the transaction must be non-null. 
+	// The description of the transaction must be non-null. 
 	// The date must be non-null
 	// The category of this must be non-null.
 	// The memo of the transaction must be non-null.
 	
 	// RI: location != null, date != null, category != null, memo != null,
-	
 	
 	/**
 	 * Creates a new immutable transaction object using the builder object.
@@ -25,20 +28,21 @@ public class Transaction {
 	 * necessary to create a transaction object.
 	 */
 	private Transaction(TransactionBuilder builder) {
-		this.location = builder.location;
+		this.description = builder.description;
 		this.amount = builder.amount;
 		this.date = builder.date;
 		this.category = builder.category;
 		this.memo = builder.memo;
 		
+		// Make sure the representation invariant is satisfied.
 		this.checkRep();
 	}
 	
 	/**
-	 * @return The location of where this transaction was made.
+	 * @return The description of this transaction.
 	 */
-	public String getLocation() {
-		return this.location;
+	public String getDescription() {
+		return this.description;
 	}
 	
 	/**
@@ -56,16 +60,9 @@ public class Transaction {
 	}
 	
 	/**
-	 * @return Whether this transaction was a withdrawal.
-	 */
-	public boolean isWithdrawal() {
-		return this.amount < 0.0;
-	}
-	
-	/**
 	 * @return The category of the this transaction.
 	 */
-	public CategoryTag getCategory() {
+	public String getCategory() {
 		return this.category;
 	}
 	
@@ -78,15 +75,14 @@ public class Transaction {
 	
 	
 	/**
-	 * A builder object for the Transaction object.
+	 * A builder object for Transaction.
 	 * @author L. James Davidson
 	 */
 	public static class TransactionBuilder {
 		
 		private double amount;
-		private String location;
-		private boolean isWithdrawal;
-		private CategoryTag category;
+		private String description;
+		private String category;
 		private String memo;
 		private LocalDate date;
 		
@@ -96,47 +92,42 @@ public class Transaction {
 		 * Transaction object may be built.
 		 */
 		public TransactionBuilder() {
-			
 		}
 		
 		/**
-		 * Sets the location of the this transaction.
-		 * @param loc is the location of the of transaction.
-		 * @modifies The location of the transaction.
-		 * @effects Sets the location of this transaction to the parameter.
+		 * Sets the description of this transaction.
+		 * @param descr is the description of the transaction.
 		 * @throws IllegalArgumentException if the parameter is null.
-		 * @return True if the location was successfully set to this.
 		 */
-		public boolean setLocation(String loc) {
-			if (loc == null) {
+		public void setDescription(String descr) {
+			if (descr == null) {
 				throw new IllegalArgumentException("The location of the "
 						+ "transaction must be non-null.");
-			} else {
-				this.location = loc;
-				return true;
-			}
-		}
-		
-		/**
-		 * @return The location of the transaction.
-		 */
-		public String getLocation() {
-			return this.location;
-		}
-		
-		/**
-		 * Sets the amount of the this to the parameter.
-		 * @param amount is the amount that was transfered during this transaction.
-		 * @return True if the amount of this was successfully set.
-		 */
-		public boolean setAmount(double amount) {
-			if (this.isWithdrawal) {
-				this.amount = -1 * amount;
-			} else {
-				this.amount = amount;
 			}
 			
-			return true;
+			this.description = descr;
+		}
+		
+		/**
+		 * @return The description of the transaction.
+		 */
+		public String getDescription() {
+			return this.description;
+		}
+		
+		/**
+		 * Sets the amount of the this transaction. If this transaction's category is a deposit
+		 * then the amount will be made positive and otherwise will be made negative.
+		 * @param amount is the amount that was transfered during this transaction.
+		 */
+		public void setAmount(double amount) {
+			// If this transaction is explicitly said to be a deposit then make amount positive,
+			// otherwise change amount to be negative.
+			if (this.category != null && this.category.equals("Deposit")) {
+				this.amount = Math.abs(amount);
+			} else {
+				this.amount = -1 * Math.abs(amount);
+			}
 		}
 		
 		/**
@@ -149,11 +140,14 @@ public class Transaction {
 		/**
 		 * Sets the date of this transaction.
 		 * @param date is the LocalDate object representing the date of the transaction.
-		 * @return True if the date was set successfully.
+		 * @throws IllegalArgumentException if date is null.
 		 */
-		public boolean setDate(LocalDate date) {
+		public void setDate(LocalDate date) {
+			if (date == null) {
+				throw new IllegalArgumentException("The date of a transaction cannot be null.");
+			}
+			
 			this.date = date;
-			return true;
 		}
 		
 		/**
@@ -163,60 +157,24 @@ public class Transaction {
 			return this.date;
 		}
 		
-		public boolean setAsWithdrawal(boolean isWithdrawal) {
-			this.isWithdrawal = isWithdrawal;
-			
-			// Update the amount of the transaction if necessary.
-			if ((this.isWithdrawal && this.amount > 0) ||
-				 !this.isWithdrawal && this.amount < 0) {
-				this.amount *= -1;
+		/**
+		 * Sets the category type of this transaction.
+		 * @param category is the category of expense of this transaction.
+		 * @modifies The amount of this transaction to either positive (if the category is a
+		 * 		deposit) or negative (any other category).
+		 * @throws IllegalArgumentException if category is null.
+		 */
+		public void setCategory(String category) {
+			if (category == null) {
+				throw new IllegalArgumentException("The category of a transaction "
+						+ "cannot be null.");
 			}
 			
-			return true;
-		}
-		
-		/**
-		 * @return True if this is a withdrawal and a false if this is a 
-		 * deposit.
-		 */
-		public boolean isWithdrawal() {
-			return this.isWithdrawal;
-		}
-		
-		/**
-		 * Sets the category type of this.
-		 * @param tag is the category of this.
-		 * @modifies The category of this.
-		 * @effects Sets the category to the parameter tag.
-		 * @throws InvalidArgumentException if this transaction is not a 
-		 * withdrawal.
-		 * @throws InvalidArgumentException if the parameter is null or not 
-		 * in the list of acceptable tags.
-		 * @return True if the category was successfully set.
-		 */
-		public boolean setCategory(CategoryTag tag) {
+			this.category = category;
 			
-			if (tag == null) {
-				throw new IllegalArgumentException("The category tag must "
-						+ "be non-null.");
-			} 
-			
-			if (!isWithdrawal) {
-				if (tag != CategoryTag.DEPOSIT) {
-				// If this is a deposit it must have a deposit category tag.
-				throw new IllegalArgumentException('\n' + "This transaction must have the "
-						+ "category tag of deposit since it this is a deposit.");
-				}
-
-			} else {
-				if (tag == CategoryTag.DEPOSIT) {
-				// If this is not a deposit it cannot have a category tag of deposit.
-				throw new IllegalArgumentException('\n' + "This transaction is not a deposit "
-						+ "and therefore cannot have a category tag of deposit.");
-				}
-			}
-			this.category = tag;
-			return true;
+			// Resetting the amount so that if this transactions amount is positive only if 
+			// it is a deposit.
+			this.setAmount(this.amount);
 		}
 		
 		/**
@@ -229,18 +187,14 @@ public class Transaction {
 		/**
 		 * Sets the memo line of this transaction.
 		 * @param memo is the memo string for this transaction.
-		 * @modifies The memo for this.
-		 * @effects Sets the transaction memo to the parameter.
 		 * @throws An InvalidArgumentException if memo is null.
-		 * @return True if the memo was successfully set.
 		 */
-		public boolean setMemo(String memo) {
+		public void setMemo(String memo) {
 			if (memo == null) {
 				throw new IllegalArgumentException("The memo cannot be null.");
-			} else {
-				this.memo = memo;
-				return true;
 			}
+			
+			this.memo = memo;
 		}
 		
 		/**
@@ -258,37 +212,14 @@ public class Transaction {
 		public Transaction build() {
 			return new Transaction(this);
 		}
-		
-	}
-	
-	/**
-	 * All the category types a transaction may have.
-	 * @author L. James Davidson
-	 */
-	public enum CategoryTag {
-		FOOD, HOUSING, SCHOOL, TRANSPORTATION, DEPOSIT
-	}
-
-	/**
-	 * @return an array containing the constants of this enum type, in the order they are declared.
-	 */
-	public static CategoryTag[] values() {
-		CategoryTag[] tags = new CategoryTag[5];
-		tags[0] = CategoryTag.FOOD;
-		tags[1] = CategoryTag.HOUSING;
-		tags[2] = CategoryTag.SCHOOL;
-		tags[3] = CategoryTag.TRANSPORTATION;
-		tags[4] = CategoryTag.DEPOSIT;
-		
-		return tags;
 	}
 	
 	/**
 	 * Ensures that the representation invariant is preserved.
 	 */
 	private void checkRep() {
-		// The location of the transaction must be non-null.
-		assert(this.location != null) : "The location must be non-null.";
+		// The description of the transaction must be non-null.
+		assert(this.description != null) : "The description must be non-null.";
 		
 		// The date must be non-null		
 		assert (this.date != null) : "The date for this transaction must be non-null";
